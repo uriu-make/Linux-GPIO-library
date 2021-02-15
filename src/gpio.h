4452743981fce5104452fbe701760c819895f10b
uriu-make <yoshinori.3775@outlook.jp>
2021-02-15 09:27:23 +0900

    update

--- src/gpio.h
+++ src/gpio.h
@@ -1,9 +1,12 @@
 #ifndef GPIO_H_INCLUDE
 #define GPIO_H_INCLUDE
+#include <linux/gpio.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
-#include <unistd.h>
-#include <asm/ioctl.h>
 #include <string.h>
+#include <unistd.h>
 
 #endif
