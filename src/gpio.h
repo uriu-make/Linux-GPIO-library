3158d6382041598e219a01b46853c4f78517e68c
uriu-make <yoshinori.3775@outlook.jp>
2021-02-15 13:28:23 +0900

    update

--- src/gpio.h
+++ src/gpio.h
@@ -8,5 +8,13 @@
 #include <sys/ioctl.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
+int gpioSetup(char *device);
+int pinMode(int pin, int mode);
+int setupParallel(int No_pins, int mode, int pin, ...);  //No. of pins. return parallel fd
+int digitalWrite(int pin, int value);
+int digitalRead(int pin);
+int ParallelWrite(int fd, char *value);
+int ParallelRead(int fd);
 #endif
