271425198791ddd8a42ca9dd80a45f32a6e0a738
uriu-make <yoshinori.3775@outlook.jp>
2021-02-17 10:28:17 +0900

    update

--- example/paralleltest.cpp
+++ example/paralleltest.cpp
@@ -13,7 +13,8 @@ int main(void) {
   for (int i; i < 10; i++) {
     gpiochip0.ParallelWrite(fd1, value);
     gpiochip0.ParallelRead(fd2, data);
-    printf("output:%d,%d,%d,%d input:%d,%d,%d,%d\n", value[0], value[1], value[2], value[3], data[0], data[1], data[2], data[3]);
+    printf("output:%d,%d,%d,%d input:%d,%d,%d,%d\n", value[0], value[1], value[2], value[3],
+           data[0], data[1], data[2], data[3]);
     for (int i = 0; i < 4; i++)
       value[i] = !value[i];
     usleep(1000000);
