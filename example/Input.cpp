87661dab64549d360b451457679627e831442281
uriu-make <yoshinori.3775@outlook.jp>
2021-03-02 01:14:49 +0900

    upload example

---
+++ example/Input.cpp
@@ -0,0 +1,17 @@
+#include <gpiodev.h>
+#include <unistd.h>
+
+gpio gpiochip0("/dev/gpiochip0");
+
+int main(void) {
+  gpiochip0.gpioSetup();
+  gpiochip0.pinMode(21, INPUT_PULLUP);
+
+  for (int i = 0; i < 10000; i++) {
+    printf("%d\n", gpiochip0.digitalRead(21));
+    usleep(1000);
+  }
+  
+  gpiochip0.Closedev();
+  return 0;
+}
