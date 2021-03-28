67d156f91856a55602c0e87a6c766ed4663cd3fa
uriu-make <yoshinori.3775@outlook.jp>
2021-03-29 00:30:36 +0900

    fix

--- example/Input.cpp
+++ example/Input.cpp
@@ -5,7 +5,7 @@ gpio gpiochip0("/dev/gpiochip0");
 
 int main(void) {
   gpiochip0.gpioSetup();
-  gpiochip0.pinMode(21, INPUT_PULLUP);
+  gpiochip0.pinMode(21, INPUT);
 
   for (int i = 0; i < 10000; i++) {
     printf("%d\n", gpiochip0.digitalRead(21));
