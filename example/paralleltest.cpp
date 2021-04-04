32d28cc15c51413b32b15e99347dcbfd95bb0a3c
uriu-make <yoshinori.3775@outlook.jp>
2021-04-04 18:49:09 +0900

    update

--- example/paralleltest.cpp
+++ example/paralleltest.cpp
@@ -4,7 +4,8 @@
 
 gpio gpiochip0("/dev/gpiochip0");
 
-void dec2bin(const int value);
+int dec2bin(const int value);
+
 int main(void) {
   gpiochip0.gpioSetup();
   int fd1 = gpiochip0.setupParallelOut(4, 20, 21, 22, 23);
@@ -12,15 +13,22 @@ int main(void) {
 
   int value = 0b1010;
   int data = 0;
-  for (int i; i < 10; i++) {
+  for (int i = 0; i < 10; i++) {
     gpiochip0.ParallelWrite(fd1, value);
     data = gpiochip0.ParallelRead(fd2);
-    printf("output:%d,input:%d\n", value, data);
-    value = !value;
+    printf("output:0b%d,input:0b%d\n", dec2bin(value), dec2bin(data));
+    value = ~value & 0b1111;
     usleep(1000000);
   }
   gpiochip0.CloseSpecialIO(fd1);
   gpiochip0.CloseSpecialIO(fd2);
   gpiochip0.Closedev();
   return 0;
+}
+
+int dec2bin(int value) {
+  int bin = 0, length = sizeof(int);
+  for (int i = 1; i <= length * 8; i++)
+    bin = bin * 10 + ((((unsigned int)value >> length * 8 - i) & 0b00000001));
+  return bin;
 }
